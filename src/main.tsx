1483e6854968a701c1af2403f991eb7cdd14b80c
adiahenot-ozouaki <adiahenotozouaki@gmail.com>
2026-09-06 10:10:49 +0100

    chore: import screens-misc.css

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './screens/screens-lms.css'
 import './screens/screens-rules.css'
 import './screens/screens-game.css'
 import './screens/screens-game-table.css'
+import './screens/screens-misc.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
