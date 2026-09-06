00209c2a48ec12d26e3cdd711b2f4aa4618e74a2
adiahenot-ozouaki <adiahenotozouaki@gmail.com>
2026-09-06 08:42:43 +0100

    chore: import rules + perf CSS in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from './App'
 import './theme/theme.css'
 import './index.css'
 import './theme/theme-bridge.css'
+import './theme/perf.css'
 import './screens/screens.css'
 import './screens/screens-lms.css'
+import './screens/screens-rules.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
