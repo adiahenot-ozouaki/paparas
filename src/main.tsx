5d6ec86c827e7b2b16f93b2b2770e21870bbc610
adiahenot-ozouaki <adiahenotozouaki@gmail.com>
2026-09-06 08:35:39 +0100

    ui: CSS for Lobby, GameMode, Stats layouts

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './theme/theme.css'
 import './index.css'
 import './theme/theme-bridge.css'
 import './screens/screens.css'
+import './screens/screens-lms.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
