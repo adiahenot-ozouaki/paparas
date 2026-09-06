8c565c2d0b5c47d5bc4200bef3c7cd7785fbb269
adiahenot-ozouaki <adiahenotozouaki@gmail.com>
2026-09-06 06:46:02 +0100

    Import theme-bridge.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './theme/theme.css'
 import './index.css'
+import './theme/theme-bridge.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
